f27bfd8f1e2b18fb48ba286faa53513af67d5259
Tom Kwok <work@tomkwok.xyz>
2024-08-13 23:01:47 +1000

    Add LLM

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -1,83 +1,121 @@
-import {FireCrawlLoader} from "@langchain/community/document_loaders/web/firecrawl";
-import {MarkdownTextSplitter} from "@langchain/textsplitters";
-import {Document} from "@langchain/core/documents";
-import {ChatOpenAI, OpenAIEmbeddings} from "@langchain/openai";
-import {MemoryVectorStore} from "langchain/vectorstores/memory";
-import {ChatPromptTemplate, PromptTemplate} from "@langchain/core/prompts";
-import {createStuffDocumentsChain} from "langchain/chains/combine_documents";
-import {StringOutputParser} from "@langchain/core/output_parsers";
-import {z} from "zod";
-import {StructuredOutputParser} from "langchain/output_parsers";
+import { MarkdownTextSplitter } from "@langchain/textsplitters";
+import { Document } from "@langchain/core/documents";
+import { ChatOpenAI, OpenAIEmbeddings } from "@langchain/openai";
+import { MemoryVectorStore } from "langchain/vectorstores/memory";
+import { ChatPromptTemplate } from "@langchain/core/prompts";
+import FirecrawlApp, { ScrapeResponse } from "@mendable/firecrawl-js";
+import { StringOutputParser } from "@langchain/core/output_parsers";
+import {
+  RunnableSequence,
+  RunnablePassthrough,
+} from "@langchain/core/runnables";
+import { VectorStoreRetriever } from "@langchain/core/vectorstores";
 
+export async function loadUrl(url: string) {
+  const crawlLoader: FirecrawlApp = new FirecrawlApp({
+    apiKey: process.env.FIRECRAWL_API_KEY,
+  });
 
-export async function loadUrl(url: string, mode: "scrape" | "crawl" = "scrape") {
-    const loader = new FireCrawlLoader({
-        url: url,
-        apiKey: process.env.FIRECRAWL_API_KEY,
-        mode: mode,
-        params: {
-            onlyMainContent: false,     // Only return the main content of the page, excluding the header, footer, etc.
-            includeHTML: true,       // Include the HTML content of the page, this will add an `html` field to the response
-            screenshot: true,     // Getting the screenshot of the top of the page that is being scraped
-        }
-    })
+  const scrapeResponse: ScrapeResponse = await crawlLoader.scrapeUrl(url, {
+    pageOptions: {
+      includeHtml: true,
+      replaceAllPathsWithAbsolutePaths: true,
+      fullPageScreenshot: true,
+      waitFor: 3000,
+    },
+  });
 
-    const document = await loader.load()
-
-    console.log(`Metadata: ${document[0].metadata}`)
-    return {
-        pageContent: document[0].pageContent,
-        metadata: document[0].metadata
-    }
+  return {
+    content: scrapeResponse.data.content,
+    markdown: scrapeResponse.data.content,
+    html: scrapeResponse.data.content,
+    linksOnPage: scrapeResponse.data.linksOnPage,
+    metadata: {
+      title: scrapeResponse.data.metadata.title,
+      description: scrapeResponse.data.metadata.description,
+      ogTitle: scrapeResponse.data.metadata.ogTitle,
+      ogDescription: scrapeResponse.data.metadata.ogDescription,
+      ogUrl: scrapeResponse.data.metadata.ogUrl,
+      ogImage: scrapeResponse.data.metadata.ogImage,
+      ogLocaleAlternate: scrapeResponse.data.metadata.ogLocaleAlternate,
+      screenshot: scrapeResponse.data.metadata.screenshot,
+      sourceUrl: scrapeResponse.data.metadata.sourceUrl,
+      pageStatusCode: scrapeResponse.data.metadata.pageStatusCode,
+    },
+  };
 }
 
-export async function splitMarkdownDocument(content: string, chunkSize: number, chunkOverlap: number) {
-    const splitter = new MarkdownTextSplitter({
-        chunkSize: chunkSize,
-        chunkOverlap: chunkOverlap
-    })
+export async function splitMarkdownDocument(
+  content: string,
+  chunkSize: number,
+  chunkOverlap: number,
+) {
+  // TODO: Try semantic chunking https://github.com/pinecone-io/examples/blob/master/learn/generation/better-rag/02b-semantic-chunking.ipynb
 
-    const splitDocuments = splitter.createDocuments([content])
+  const splitter = new MarkdownTextSplitter({
+    chunkSize: chunkSize,
+    chunkOverlap: chunkOverlap,
+  });
 
-    console.log(await splitDocuments)
-    return await splitDocuments
+  return splitter.createDocuments([content]);
 }
 
-export async function embedDocuments(chunks: Document[], k?: number, searchType?: "similarity" | "mmr") {
-    // TODO: Options for different LLM models
-    const embedding = new OpenAIEmbeddings()
-    const vectorStore = await MemoryVectorStore.fromDocuments(chunks, embedding)
+export async function embedDocuments(
+  chunks: Document[],
+  k?: number,
+  searchType?: "similarity" | "mmr",
+) {
+  // TODO: Options for different LLM models
+  const embedding = new OpenAIEmbeddings();
+  const vectorStore = await MemoryVectorStore.fromDocuments(chunks, embedding);
 
-    const retriever = vectorStore.asRetriever()
-
-    console.log(retriever)
-
-    return retriever
+  return vectorStore.asRetriever();
 }
 
-export async function createDocumentChain(prompt: ChatPromptTemplate) {
+export async function createDocumentChain(
+  retriever: VectorStoreRetriever<MemoryVectorStore>,
+) {
+  //
+  // const zodSchema = z.object({
+  //         websiteName: z.string().describe("The name of the website"),
+  //         mainNavigationItems: z.array(z.string()).describe("The main navigation items of the website"),
+  //         summary: z.string().describe("The summary of the website's overall purpose and key contents under each" +
+  //             " navigation item"),
+  //         keyContents: z.array(z.string()).describe("The key contents under each navigation item"),
+  //         nextMove: z.string().describe("Suggest the next move for the user based on the provided content, ensuring the" +
+  //             " suggestion aligns with the website's main focus and the user's potential interests.")
+  //     }
+  // )
 
-    const zodSchema = z.object({
-            websiteName: z.string().describe("The name of the website"),
-            mainNavigationItems: z.array(z.string()).describe("The main navigation items of the website"),
-            summary: z.string().describe("The summary of the website's overall purpose and key contents under each" +
-                " navigation item"),
-            keyContents: z.array(z.string()).describe("The key contents under each navigation item"),
-            nextMove: z.string().describe("Suggest the next move for the user based on the provided content, ensuring the" +
-                " suggestion aligns with the website's main focus and the user's potential interests.")
-        }
-    )
+  // const parser = StructuredOutputParser.fromZodSchema(zodSchema)
 
-    const parser = StructuredOutputParser.fromZodSchema(zodSchema)
+  const parser = new StringOutputParser();
 
-    const llm = new ChatOpenAI({
-        modelName: "gpt-4o-mini",
-        temperature: 0
-    })
+  const llm = new ChatOpenAI({
+    modelName: "gpt-4o-mini",
+    temperature: 0,
+  });
 
-    return await createStuffDocumentsChain({
-        llm,
-        prompt: prompt,
-        outputParser: parser
-    })
+  const formatDocumentsAsString = (documents: Document[]) => {
+    return documents.map((document) => document.pageContent).join("\n\n");
+  };
+
+  const SYSTEM_TEMPLATE: string = `You are a helpful assistant helping visually impaired user to interpret and navigate the web. You will be given context of the webpage that the user is visiting, and answer their questions to the best of your ability using only the resources provided. Be verbose, concise, friendly in your response.
+    --------------
+    {context}`;
+
+  const prompt = ChatPromptTemplate.fromMessages([
+    ["system", SYSTEM_TEMPLATE],
+    ["human", "{question"],
+  ]);
+
+  return RunnableSequence.from([
+    {
+      context: retriever.pipe(formatDocumentsAsString),
+      question: new RunnablePassthrough(),
+    },
+    prompt,
+    llm,
+    parser,
+  ]);
 }
